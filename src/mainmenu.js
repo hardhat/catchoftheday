ab9fc5ebc96d1d92bef3fa77deec26764ec60073
Dale Wick <dmwick@yahoo.com>
2025-05-10 10:50:46 -0400

    Add required images
    - Tune buttons on the main menu.

--- src/mainmenu.js
+++ src/mainmenu.js
@@ -1,5 +1,5 @@
 // Main menu goes here.
-
+import StylizedTextBox from "./stylizedtextbox.js";
 // Options from main menu: play, about (shows credits) as buttons.
 
 export default class MainMenu extends Phaser.Scene {
@@ -14,29 +14,53 @@ export default class MainMenu extends Phaser.Scene {
         //this.load.image('play', 'assets/play.png');
         //this.load.image('about', 'assets/about.png');
         //this.load.image('exit', 'assets/exit.png');
+        this.load.image('goatonapole', 'data/goatonapole.jpg');
+        this.load.image('tojam2025', 'data/tojam2025.png');
     }
 
     create() {
-        this.createFancyText(400, 100, 'Catch of the Day', 32);
-        const playButton = this.createFancyText(400, 200, 'Play', 32).setInteractive().on('pointerdown', () => {
+        const box = new StylizedTextBox(this, 50, 50, '', 700, 500);
+        this.createFancyText(200, 75, 'Catch of the Day', 48);
+        const playButton = this.createFancyText(400, 200, ' Play ', 32)
+            .setStyle({backgroundColor: '0x463829'})
+            .setInteractive()
+            .on('pointerdown', () => {
             this.gamemode = true;
             this.scene.start('GameScene', { gamemode: this.gamemode });
-        });
-        const aboutButton = this.createFancyText(400, 300, 'About', 32).setInteractive().on('pointerdown', () => {
+            })
+            .on('pointerover', () => {
+            playButton.setStyle({ backgroundColor: '#ffff80' });
+            })
+            .on('pointerout', () => {
+            playButton.setStyle({ backgroundColor: '0x463829' });
+            });
+
+        const aboutButton = this.createFancyText(400, 300, ' About ', 32)
+            .setStyle({backgroundColor: '0x463829'})
+            .setInteractive()
+            .on('pointerdown', () => {
             this.scene.start('AboutScene');
-        });
+            })
+            .on('pointerover', () => {
+            aboutButton.setStyle({ backgroundColor: '#ffff80' });
+            })
+            .on('pointerout', () => {
+            aboutButton.setStyle({ backgroundColor: '0x463829' });
+            });
         
-        this.add.image(0, 0, 'mainmenu').setOrigin(0, 0).setScale(1.5);
-        this.add.image(400, 300, 'play').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
-            this.gamemode = true;
-            this.scene.start('GameScene', { gamemode: this.gamemode });
-        });
-        this.add.image(400, 400, 'about').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
-            this.scene.start('AboutScene');
-        });
-        this.add.image(400, 500, 'exit').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
-            this.game.destroy(true);
-        });
+        this.add.image(200, 250, 'goatonapole').setScale(0.5);
+        this.add.image(400, 450, 'tojam2025').setScale(0.5);
+        // this.add.image(0, 0, 'mainmenu').setOrigin(0, 0).setScale(1.5);
+        // this.add.image(400, 300, 'play').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
+        //     this.gamemode = true;
+        //     this.scene.start('GameScene', { gamemode: this.gamemode });
+        // });
+        // this.add.image(400, 400, 'about').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
+        //     this.scene.start('AboutScene');
+        // });
+        // this.add.image(400, 500, 'exit').setOrigin(0.5, 0.5).setScale(1.5).setInteractive().on('pointerdown', () => {
+        //     this.game.destroy(true);
+        // });
     }
 
     createFancyText(x, y, message, size) {
