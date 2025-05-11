79225a3f739e7d2863195ec6e9f14af9105031b0
Dale Wick <dmwick@yahoo.com>
2025-05-11 16:36:01 -0400

    Show victory message.

--- src/escapescene.js
+++ src/escapescene.js
@@ -1,3 +1,5 @@
+import StylizedTextBox from "./stylizedtextbox.js";
+
 export default class EscapeScene extends Phaser.Scene {
     constructor() {
         super('EscapeScene');
@@ -21,7 +23,7 @@ export default class EscapeScene extends Phaser.Scene {
         });
 
         // Create boat (blue circle)
-        const boat = this.add.circle(274, 128, 5, 0x0000ff);
+        const boat = this.add.circle(274*2, 128*2, 5, 0x0000ff);
 
         // Create timeline for boat movement
         const timeline = this.tweens.timeline({
@@ -29,24 +31,36 @@ export default class EscapeScene extends Phaser.Scene {
             loop: false,
             tweens: [
                 {
-                    x: 276,
-                    y: 80,
+                    x: 276*2,
+                    y: 80*2,
                     duration: 5000,
                     ease: 'Linear'
                 },
                 {
-                    x: 135,
-                    y: 80,
+                    x: 135*2,
+                    y: 80*2,
                     duration: 5000,
                     ease: 'Linear'
                 },
                 {
-                    x: 103,
-                    y: 130,
+                    x: 103*2,
+                    y: 130*2,
                     duration: 5000,
                     ease: 'Linear'
                 }
             ]
+        });
+        timeline.play();
+        // Add a text box with "you escaped!" message
+        const textBox = new StylizedTextBox(this, 50, 50, 'You escaped!', 700, 500);
+        textBox.setVisible(false); // Hide it initially
+
+        // Show the text box when the boat reaches the last point
+        timeline.on('complete', () => {
+            textBox.setVisible(true);
+            this.time.delayedCall(2000, () => {
+                textBox.setVisible(false);
+            }, [], this);
         });
     }
 }
